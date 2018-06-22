241a8a446c2431d1b9495ad4aac22eb9e4e644b7
abelass <rainer@websimple.be>
2018-06-22 16:28:37 +0200

    calcul faux

--- prix_objet/po_periode.php
+++ prix_objet/po_periode.php
@@ -40,6 +40,66 @@ function prix_objet_po_periode_dist($id_po_periode, $prix, $contexte = array(), 
 	$operateur_2 = !empty($donnees_periode['operateur_2']) ? $donnees_periode['operateur_2'] : '==';
 	$date_debut_periode = $donnees_periode['date_debut'];
 	$date_fin_periode = $donnees_periode['date_fin'];
+	$jour_debut_periode = $donnees_periode['jour_debut'];
+	$jour_fin_periode = $donnees_periode['jour_fin'];
+
+	if ($mode == 'prorata' and is_array($sequence)) {
+		$prix_prorata = isset($contexte['prix_prorata']) ? $contexte['prix_prorata'] : array();
+
+		foreach ($sequence as $index => $date_sequence) {
+			if ($prix_applicable = _po_obtenir_prix(
+					$date_sequence,
+					$date_sequence,
+					$date_debut_periode,
+					$date_fin_periode,
+					$jour_debut_periode,
+					$jour_fin_periode,
+					$prix,
+					$type,
+					$criteres,
+					$operateur,
+					$operateur2)) {
+				$prix_prorata[$date_sequence] = $prix_applicable;
+				unset($sequence[$index]);
+			}
+		}
+		$prix_applicable = array(
+			'sequence' => $sequence,
+			'prix_prorata' => $prix_prorata,
+		);
+	}
+	else {
+		$prix_applicable = _po_obtenir_prix(
+			$date_debut_contexte,
+			$date_fin_contexte,
+			$date_debut_periode,
+			$date_fin_periode,
+			$jour_debut_periode,
+			$jour_fin_periode,
+			$prix,
+			$type,
+			$criteres,
+			$operateur,
+			$operateur2);
+	}
+
+
+	return $prix_applicable;
+}
+
+function _po_obtenir_prix(
+	$date_debut_contexte,
+	$date_fin_contexte,
+	$date_debut_periode,
+	$date_fin_periode,
+	$jour_debut_periode,
+	$jour_fin_periode,
+	$prix,
+	$type,
+	$criteres,
+	$operateur,
+	$operateur2) {
+	$prix_applicable = '';
 
 	switch ($type) {
 		case 'date':
@@ -55,15 +115,14 @@ function prix_objet_po_periode_dist($id_po_periode, $prix, $contexte = array(), 
 					}
 					break;
 				case 'specifique':
-					if (po_condition($date_debut_contexte, $date_debut_periode, $operateur) and po_condition($date_fin_contexte, $date_fin_periode, $operateur_2)) {
+					if (_po_condition($date_debut_contexte, $date_debut_periode, $operateur) and
+							_po_condition($date_fin_contexte, $date_fin_periode, $operateur_2)) {
 						$prix_applicable = $prix;
 					}
 					break;
 			}
 			break;
 		case 'jour_semaine':
-			$jour_debut_periode = $donnees_periode['jour_debut'];
-			$jour_fin_periode = $donnees_periode['jour_fin'];
 			$jour_debut_contexte = date('w', strtotime($date_debut_contexte));
 			$jour_fin_contexte = date('w', strtotime($date_fin_contexte));
 			$dates_periode = array(
@@ -72,7 +131,11 @@ function prix_objet_po_periode_dist($id_po_periode, $prix, $contexte = array(), 
 			);
 			$dates_intervalle = dates_intervalle($date_debut_contexte, $date_fin_contexte, 0, 0, FALSE, 'w');
 			$coincidences = array_intersect($dates_periode, $dates_intervalle);
-
+			spip_log('$dates_intervalle', 'teste');
+			spip_log($dates_intervalle, 'teste');
+			spip_log('$dates_periode', 'teste');
+			spip_log($dates_periode, 'teste');
+			spip_log(count($coincidences), 'teste');
 			switch ($criteres) {
 				case 'coincide':
 					if (count($coincidences) > 0) {
@@ -99,7 +162,7 @@ function prix_objet_po_periode_dist($id_po_periode, $prix, $contexte = array(), 
 				$nombre_jours_contexte = $difference_date / (60 * 60 * 24);
 				$nombre_jours = $donnees_periode['jour_nombre'];
 
-				if (po_condition($nombre_jours_contexte, $nombre_jours, $operateur)) {
+				if (_po_condition($nombre_jours_contexte, $nombre_jours, $operateur)) {
 					$prix_applicable = $prix;
 				}
 			}
@@ -122,7 +185,7 @@ function prix_objet_po_periode_dist($id_po_periode, $prix, $contexte = array(), 
  *
  * @return boolean True si le teste est positive.
  */
-function po_condition($valeur1, $valeur2, $operateur) {
+function _po_condition($valeur1, $valeur2, $operateur) {
 	switch ($operateur) {
 		case "=":
 			return $valeur1 == $valeur2;
